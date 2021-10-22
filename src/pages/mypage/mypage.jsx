3897678e7f3797697e3e3ec54c223edc934abb76
이정민 <leejungmin@MacBook-Pro.local>
2021-10-22 15:53:06 +0900

    test

--- src/pages/mypage/mypage.jsx
+++ src/pages/mypage/mypage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Button } from "./mypageStyles";
 
-const Mypage = (props) => <h1>My page</h1>;
+const Mypage = (props) => {
+    return (
+        <>
+            <Button>My page</Button>
+            <h1>My page</h1>
+            <h1>My page</h1>
+            <h1>My page</h1>
+        </>
+    )
+}
+
+
 
 export default Mypage;
-<h1>My page</h1>;
+
---
+++ src/pages/mypage/mypageStyles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.div`
+    display: flex;
+    height: 50px;
+    width: 50px;
+    border: 1px solid red
+`;
+
+
+
