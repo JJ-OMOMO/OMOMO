4d846f2d51429fc02f4b6387568b87c34a47215a
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-10-24 20:24:56 +0900

    Profile Modal Layout

--- src/components/profile_modal/profile.jsx
+++ src/components/profile_modal/profile.jsx
@@ -1,5 +1,74 @@
 import React from "react";
+import styled from "styled-components";
+import OMO from "../../images/OMO.png";
 
-const Profile = (props) => <h1>Profile</h1>;
+const ProfileModal = (props) => {
+  return (
+    <ModalBackground>
+      <ModalContainer>
+        <BackButton>&lt;</BackButton>
+        <Profile>
+          <img src={OMO} alt="프로필 사진" width="200px" height="200px" />
+          <input tpye="text" value="닉네임" readOnly></input>
+        </Profile>
+        <CharacterList>캐릭터List</CharacterList>
+        <SaveButton>저장</SaveButton>
+      </ModalContainer>
+    </ModalBackground>
+  );
+};
 
-export default Profile;
+const ModalBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: yellow;
+`;
+
+const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1000px;
+  height: 800px;
+  background-color: white;
+`;
+
+const BackButton = styled.div`
+  font-size: 20px;
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid blue;
+  & > input {
+    margin-top: 20px;
+    width: 200px;
+  }
+`;
+
+const CharacterList = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  height: 300px;
+  border: 1px solid red;
+  margin-top: 20px;
+`;
+
+const SaveButton = styled.button`
+  width: 80px;
+  height: 30px;
+  margin-top: 20px;
+`;
+
+export default ProfileModal;
