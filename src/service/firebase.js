eb3a28064f74d1abc30e890af2a0a9b86c206a3f
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-10-27 17:49:58 +0900

    firebase db setup

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,10 +1,13 @@
 import firebase from "firebase";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-export default firebaseApp;
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const dbService = firebase.firestore();
+// dbService.settings({ timestampsInSanpshots: true });
